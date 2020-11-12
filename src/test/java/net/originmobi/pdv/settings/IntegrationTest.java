4a61bc60583b3e4ebe95b70c291ca8ace5dc5fa5
Jônathas Assunção <jonathas.alves@pontta.com>
2020-11-12 16:05:02 -0300

    test: Add test configuration

--- src/test/java/net/originmobi/pdv/settings/IntegrationTest.java
+++ src/test/java/net/originmobi/pdv/settings/IntegrationTest.java
@@ -1,25 +1,40 @@
-package net.originmobi.pdv.core;
+package net.originmobi.pdv.settings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Rollback
-@WebMvcTest
 @Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("dev")
-public abstract class IntegrationTest {
+@ExtendWith(SpringExtension.class)
+public class IntegrationTest {
 
+  @Autowired
+  private MockMvc mockMvc;
   @Autowired
   private ObjectMapper objectMapper;
   @PersistenceContext
   private EntityManager entityManager;
+
+  protected MockMvc mockMvc() {
+    return mockMvc;
+  }
+
+  protected String json(Object object) throws JsonProcessingException {
+    return objectMapper.writeValueAsString(object);
+  }
 
   protected <T> T save(T entity) {
     entityManager.persist(entity);
--- src/test/java/net/originmobi/pdv/settings/UnitTest.java
+++ src/test/java/net/originmobi/pdv/settings/UnitTest.java
@@ -1,10 +1,12 @@
-package net.originmobi.pdv.core;
+package net.originmobi.pdv.settings;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public abstract class UnitTest {
