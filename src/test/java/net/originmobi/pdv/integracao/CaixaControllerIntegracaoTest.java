696e3b19284c24d82039cbb77639dcbbb8dd2184
gabrielcs23 <gabrielcs23@hotmail.com>
2021-09-12 19:31:50 -0300

    configurações básicas para um teste de integração

---
+++ src/test/java/net/originmobi/pdv/integracao/CaixaControllerIntegracaoTest.java
@@ -0,0 +1,35 @@
+package net.originmobi.pdv.integracao;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class CaixaControllerIntegracaoTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockUser(roles = "LISTAR_CAIXA")
+    void testeSimples() throws Exception {
+
+        mvc.perform(get("/caixa"))
+                .andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(model().attribute("caixas"));
+    }
+
+
+}
